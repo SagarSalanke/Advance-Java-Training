4d1d8771daaffb12b6d47d62aaed0646c6c64bb8
Sagar Rao Salanke P <sagarsalanke2@gmail.com>
2022-04-28 15:22:54 +0530

    Advance Training

---
+++ Problem Statement 8/ProblemStatement8.3/src/com/textfiles/TransactionWithdraw.java
@@ -0,0 +1,16 @@
+package com.textfiles;
+
+public class TransactionWithdraw implements Runnable{
+	int amount;
+	Account accountY;
+	
+	TransactionWithdraw(Account y, int amount) {
+		accountY = y;
+		this.amount = amount;
+		new Thread(this).start();
+	}
+	
+	public void run(){
+		accountY.withdraw(amount);
+	}
+}
